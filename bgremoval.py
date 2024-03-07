644bdb2915f447670661cad6a30f678780016117
Akash265 <akash265457k@gmail.com>
2024-03-07 19:03:49 +0530

    first commit

---
+++ bgremoval.py
@@ -0,0 +1,70 @@
+from PIL import Image
+import cv2
+import numpy as np
+import glob
+
+def find_exterior_edges(image_path):
+    # Load the image
+    img = Image.open(image_path)
+
+    # Convert the image to grayscale
+    gray_img = img.convert("L")
+
+    # Convert PIL Image to numpy array
+    np_img = np.array(gray_img)
+
+    np_blurred = cv2.GaussianBlur(np_img, (5, 5), 0)
+   
+    # apply morphology
+    # Apply adaptive thresholding
+    binary = cv2.adaptiveThreshold(np_blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+   
+    # Find contours
+    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Create a mask for exterior edges
+    exterior_edges_mask = np.zeros_like(binary)
+    
+    # Draw filled contours on the mask
+    cv2.drawContours(exterior_edges_mask, contours, -1, (255), thickness=cv2.FILLED)
+
+    # Apply erosion and dilation for refining the contour
+    kernel = np.ones((9, 9), np.uint8)
+    exterior_edges_mask = cv2.erode(exterior_edges_mask, kernel, iterations=2)
+    exterior_edges_mask = cv2.dilate(exterior_edges_mask, kernel, iterations=1)
+
+    #  Smoothen the mask
+    smooth_mask = cv2.medianBlur(exterior_edges_mask, 9)
+    smooth_mask = cv2.GaussianBlur(smooth_mask, (3, 3), 0)
+
+    return smooth_mask
+
+def remove_background(image_path, edges_mask):
+    # Load the image
+    img = Image.open(image_path)
+
+    # Convert the image to RGBA mode
+    img = img.convert("RGBA")
+
+    # Invert the edges mask
+    inverted_edges_mask = 255 - edges_mask
+
+    # Convert the inverted edges mask to PIL Image
+    inverted_edges_img = Image.fromarray(inverted_edges_mask, mode='L')
+
+    # Create a new image with alpha channel
+    new_img = Image.new("RGBA", img.size, (255, 255, 255, 0))
+
+    # Composite the original image and the inverted contour mask
+    new_img.paste(img, (0, 0), img)
+    new_img.paste((255, 255, 255, 0), (0, 0), inverted_edges_img)
+
+    return new_img
+
+image_dir='Images-1'
+output_dir='Output-1'
+
+for image_path in glob.glob(image_dir+"/**"):
+    output_img=remove_background(image_path=image_path,edges_mask=find_exterior_edges(image_path))
+    output_img.save(output_dir+f'/'+image_path.split('/')[-1].split('.')[0] +'.png')
+    
